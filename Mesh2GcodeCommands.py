4c541538a60bce58d653669e0a934e96ccde2298
HTXF-g <111024448+HTXF-g@users.noreply.github.com>
2023-05-20 20:31:27 +0200

    writes 2 gcode file

--- Mesh2GcodeCommands.py
+++ Mesh2GcodeCommands.py
@@ -11,11 +11,13 @@ with open("goodMesh.txt", "r") as f:
 	f.seek(0)
 	for y in range(9,-1,-1):	#2D Array füllen
 		Matrix[y] = f.readline().removesuffix("\n").split("\t")
+		print(Matrix[y])
 	
-for y in range(sizeXY-1,-1,-1):
-	#print( str(y) + ":", end="")
-	for x in range(sizeXY):
-		#print(str(x) + " ", end="")
-		#M421 I3 J3 Z0.0000
-		print("M421", "I" + str(x), "J" + str(y), "Z" + str(Matrix[y][x]))
-	#print()
+with open("setUBL-Mesh.gcode", "w", encoding="utf-8") as f:
+	for y in range(sizeXY-1,-1,-1):
+		for x in range(sizeXY):
+			#M421 I3 J3 Z0.0000
+			line = "M421 I" + str(x) + " J" + str(y) + " Z" + str(Matrix[y][x]).replace(",", ".")
+			print(line)
+			f.write(line + "\n")
+	
